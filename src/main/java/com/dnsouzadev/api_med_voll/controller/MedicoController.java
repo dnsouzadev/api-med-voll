0cbab95b0e4f7da7d87e4c7a501ad5c90b859dfe
Daniel Souza <workdndsza@gmail.com>
2024-08-28 12:47:21 -0300

    criando dto para listar os dados dos medicos

--- src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
+++ src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package com.dnsouzadev.api_med_voll.controller;
 
 import com.dnsouzadev.api_med_voll.medico.CadastroMedico;
+import com.dnsouzadev.api_med_voll.medico.DadosListagemMedico;
 import com.dnsouzadev.api_med_voll.medico.Medico;
 import com.dnsouzadev.api_med_voll.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,6 +22,13 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid CadastroMedico medico) {
         repository.save(new Medico(medico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream()
+                .map(DadosListagemMedico::new)
+                .toList();
     }
 
 }
