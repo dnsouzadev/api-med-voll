9b7d0596f4e42037cba8542e342cde0bb67f7ca4
Daniel Souza <workdndsza@gmail.com>
2024-08-28 12:01:53 -0300

    validando com o bean validation

--- src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
+++ src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.dnsouzadev.api_med_voll.controller;
 import com.dnsouzadev.api_med_voll.medico.CadastroMedico;
 import com.dnsouzadev.api_med_voll.medico.Medico;
 import com.dnsouzadev.api_med_voll.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody CadastroMedico medico) {
+    public void cadastrar(@RequestBody @Valid CadastroMedico medico) {
         repository.save(new Medico(medico));
     }
 
