db473c539aeb1b0fd14d0f20a9d010a8131072d8
Daniel Souza <workdndsza@gmail.com>
2024-08-28 12:56:58 -0300

    paginando e ordenando

--- src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
+++ src/main/java/com/dnsouzadev/api_med_voll/controller/MedicoController.java
@@ -6,6 +6,9 @@ import com.dnsouzadev.api_med_voll.medico.Medico;
 import com.dnsouzadev.api_med_voll.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +28,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream()
-                .map(DadosListagemMedico::new)
-                .toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable pageable) {
+        return repository.findAll(pageable)
+                .map(DadosListagemMedico::new);
     }
 
 }
