250227611aa59a6f1a26c4d394a94d715416ea77
Daniel Souza <workdndsza@gmail.com>
2024-08-28 10:00:23 -0300

    iniciando

---
+++ src/main/java/com/dnsouzadev/api_med_voll/ApiMedVollApplication.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.api_med_voll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedVollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedVollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnsouzadev/api_med_voll/ApiMedVollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.api_med_voll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedVollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
