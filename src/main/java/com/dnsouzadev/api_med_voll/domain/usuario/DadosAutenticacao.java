62176fc19ab699532fbc797b86e05cebb3b9cfc9
Daniel Souza <workdndsza@gmail.com>
2024-09-02 13:05:02 -0300

    implementando o sistema de autenticacao

---
+++ src/main/java/com/dnsouzadev/api_med_voll/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,3 @@
+package com.dnsouzadev.api_med_voll.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {}
--- src/main/java/com/dnsouzadev/api_med_voll/infra/security/SecurityConfigurations.java
+++ src/main/java/com/dnsouzadev/api_med_voll/infra/security/SecurityConfigurations.java
@@ -1,23 +1,36 @@
 package com.dnsouzadev.api_med_voll.infra.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // Desabilita CSRF
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Define a política de criação de sessão como STATELESS
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // Exemplo de autenticação para todas as requisições
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
-        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
