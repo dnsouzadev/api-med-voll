ac9807647f44c663270cdf6bd43c1fc67b19556c
Daniel Souza <workdndsza@gmail.com>
2024-09-02 20:17:17 -0300

    gerando tokens jwt

--- src/main/java/com/dnsouzadev/api_med_voll/controller/AutenticacaoController.java
+++ src/main/java/com/dnsouzadev/api_med_voll/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package com.dnsouzadev.api_med_voll.controller;
 
 import com.dnsouzadev.api_med_voll.domain.usuario.DadosAutenticacao;
+import com.dnsouzadev.api_med_voll.domain.usuario.Usuario;
+import com.dnsouzadev.api_med_voll.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticar(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
---
+++ src/main/java/com/dnsouzadev/api_med_voll/infra/security/TokenService.java
@@ -0,0 +1,33 @@
+package com.dnsouzadev.api_med_voll.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.dnsouzadev.api_med_voll.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(java.time.ZoneOffset.of("-03:00"));
+    }
+}
