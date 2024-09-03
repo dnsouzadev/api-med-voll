c807e8b965a5ade2f33bda674477bec5ab80d111
Daniel Souza <workdndsza@gmail.com>
2024-09-02 21:46:11 -0300

    terminando o sistema de autenticacao e autorizacao

---
+++ src/main/java/com/dnsouzadev/api_med_voll/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.dnsouzadev.api_med_voll.infra.security;
+
+import com.dnsouzadev.api_med_voll.domain.usuario.UsuarioRepository;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+
+        if (tokenJwt != null) {
+            var subject = tokenService.getSubject(tokenJwt);
+            var usuario = usuarioRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var token = request.getHeader("Authorization");
+
+        return token != null ? token.replace("Bearer ", "") : null;
+
+    }
+}
--- src/main/java/com/dnsouzadev/api_med_voll/infra/security/TokenService.java
+++ src/main/java/com/dnsouzadev/api_med_voll/infra/security/TokenService.java
@@ -1,15 +1,16 @@
 package com.dnsouzadev.api_med_voll.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.dnsouzadev.api_med_voll.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Service
 public class TokenService {
@@ -28,6 +29,19 @@ public class TokenService {
         } catch (JWTCreationException exception){
             // Invalid Signing configuration / Couldn't convert Claims.
             throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token inválido ou expirado!", exception);
         }
     }
 
